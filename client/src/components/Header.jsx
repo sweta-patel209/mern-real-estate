65cb8b04c31ef2c698a39064f24ed62de8d93691
sweta.patel209 <sweta.patel2091983@gmail.com>
2023-11-26 09:19:51 +1030

    created UI for the sign up page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,36 +3,36 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-blue-300 shadow-md ">
+    <header className="bg-slate-300 shadow-md ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-blue-600">Sweta</span>
-            <span className="text-blue-700">Estate</span>
+            <span className="text-slate-600">Sweta</span>
+            <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
 
-        <form className="bg-blue-100 p-3 rounded-lg flex items-center">
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none text-blue-700 w-24 sm:w-64"
+            className="bg-transparent focus:outline-none text-slate-700 w-24 sm:w-64 "
           />
-          <FaSearch className="text-blue-600" />
+          <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-blue-700 hover:underline">
+            <li className="hidden sm:inline text-slate-700 hover:underline font-semibold">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-blue-700 hover:underline">
+            <li className="hidden sm:inline text-slate-700 hover:underline font-semibold">
               About
             </li>
           </Link>
           <Link to="/sign-in">
-            <li className="text-blue-700 hover:underline">Sign in</li>
+            <li className="text-slate-700 hover:underline font-semibold">Sign in</li>
           </Link>
         </ul>
       </div>
