b014a8cebf3d65a3babe6e268fbabd2fd286c941
sweta.patel209 <sweta.patel2091983@gmail.com>
2023-11-25 14:28:39 +1030

    implemented header for the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+
+
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
-  )
+  <BrowserRouter>
+  <Header/>  
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<Signin/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/profile" element={<Profile/>}/>    
+  </Routes>  
+  </BrowserRouter>)
+  
 }
